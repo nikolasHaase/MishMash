1c776b7e670c89fe6081083dfc6a01acde373130
Shinichi Kudo <Shinichi@shinichis-mac.local>
2016-08-26 21:26:24 +0200

    Initial Commit

---
+++ MishMash/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MishMash
+//
+//  Created by Shinichi Kudo on 8/26/16.
+//  Copyright © 2016 Nikolas Haase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
